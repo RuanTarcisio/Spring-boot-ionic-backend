d54b57258f77bc2abdde60d8eb8a95c2a6951229
RuanTarcisio <RuanTarciisio@gmail.com>
2023-07-12 13:27:30 -0300

    Criando repository e service para Categoria

--- eclipse-workspace/cursomc/src/main/java/com/example/demo/cursomc/resource/CategoriaResource.java
+++ eclipse-workspace/cursomc/src/main/java/com/example/demo/cursomc/resource/CategoriaResource.java
@@ -3,28 +3,36 @@ package com.example.demo.cursomc.resource;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.cursomc.domain.Categoria;
+import com.example.demo.cursomc.service.CategoriaService;
 
 @RestController
 @RequestMapping(value="categorias")
-class CategoriaResource {
+public class CategoriaResource {
 
-	@RequestMapping(method = RequestMethod.GET)
-	public List<Categoria> listar() {
+	@Autowired
+	private CategoriaService service;
+	
+	
+	@RequestMapping(value= "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> search (@PathVariable Integer id) {
+		Categoria obj = service.find(id); 
+//		List<Categoria> lista = new ArrayList<Categoria>();
+//		Categoria cat1 = new Categoria(1, "Informatica");
+//		Categoria cat2 = new Categoria(2, "Escritório");
+//		
+//		lista.add(cat1);
+//		lista.add(cat2);
 		
-		
-		List<Categoria> lista = new ArrayList<Categoria>();
-		Categoria cat1 = new Categoria(1, "Informatica");
-		Categoria cat2 = new Categoria(2, "Escritório");
-		
-		lista.add(cat1);
-		lista.add(cat2);
-		
-		return lista;
+		return ResponseEntity.ok().body(obj);
 		
 	}
 }
