0ea25b4320da88a7fc59f43c8ddead9751451062
Ruan Tarcisio <ruantarciisio@gmail.com>
2023-07-19 10:19:17 -0300

    Validacao Sintatica com Bean Validation

--- cursomc/src/main/java/com/example/demo/cursomc/dto/CategoriaDTO.java
+++ cursomc/src/main/java/com/example/demo/cursomc/dto/CategoriaDTO.java
@@ -2,12 +2,19 @@ package com.example.demo.cursomc.dto;
 
 import java.io.Serializable;
 
+import org.hibernate.validator.constraints.Length;
+
 import com.example.demo.cursomc.domain.Categoria;
+
+import jakarta.validation.constraints.NotEmpty;
 
 public class CategoriaDTO implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	private Integer id;
+	
+	@NotEmpty(message = "preenchimento obrigatorio")
+	@Length(min = 5, max = 80, message = "deve ser entre 5 e 80 caracteres")
 	private String nome;
 	
 	
--- cursomc/src/main/java/com/example/demo/cursomc/resource/CategoriaResource.java
+++ cursomc/src/main/java/com/example/demo/cursomc/resource/CategoriaResource.java
@@ -20,6 +20,8 @@ import com.example.demo.cursomc.domain.Categoria;
 import com.example.demo.cursomc.dto.CategoriaDTO;
 import com.example.demo.cursomc.service.CategoriaService;
 
+import jakarta.validation.Valid;
+
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
@@ -36,16 +38,20 @@ public class CategoriaResource {
 	}
 	
 	@RequestMapping(method=RequestMethod.POST)
-	public ResponseEntity<Void> insert (@RequestBody Categoria obj){
+	public ResponseEntity<Void> insert (@Valid @RequestBody CategoriaDTO objDTO){
+		
+		Categoria obj = service.fromDTO(objDTO);
 		obj = service.insert(obj);
 		
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
-				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+				.path("/{id}").buildAndExpand(objDTO.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
 	
 	@RequestMapping(value= "/{id}", method = RequestMethod.PUT)
-	public ResponseEntity<Void> update (@RequestBody Categoria obj, @PathVariable Integer id){
+	public ResponseEntity<Void> update (@Valid @RequestBody CategoriaDTO objDTO, @PathVariable Integer id){
+		
+		Categoria obj = service.fromDTO(objDTO);
 		obj.setId(id);
 		obj = service.update(obj);
 		
