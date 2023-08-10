5a17563f4396399004cef69e648863ec81cf01d2
Ruan Tarcisio <ruantarciisio@gmail.com>
2023-08-10 16:55:49 -0300

    Implementando Autorizacao

--- cursomc/src/main/java/com/example/demo/cursomc/security/JWTAuthorizationFilter.java
+++ cursomc/src/main/java/com/example/demo/cursomc/security/JWTAuthorizationFilter.java
@@ -1,6 +1,16 @@
 package com.example.demo.cursomc.security;
 
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
@@ -15,8 +25,31 @@ public class JWTAuthorizationFilter extends BasicAuthenticationFilter{
 		this.userDetailsService = userDetailsService;
 	}
 	
-	
+	protected void  doFilterInternal(HttpServletRequest request,
+									HttpServletResponse response,
+									FilterChain chain) throws IOException, ServletException{
+		
+		String header = request.getHeader("Authorization");
+		
+		if (header != null && header.startsWith("Bearer ")) {
+			UsernamePasswordAuthenticationToken auth = getAuthentication(request, header.substring(7));
+			if(auth != null) {
+				SecurityContextHolder.getContext().setAuthentication(auth);
+			}
+			chain.doFilter(request, response);
+		}
+	}
 
+	private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request, String token) {
+		if(jwtUtil.tokenValido(token)) {
+			String username = jwtUtil.getUsername(token);
+			UserDetails user = userDetailsService.loadUserByUsername(username);
+			return new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+		}
+		return null;
+	}
 	
 	
 }
+
+
